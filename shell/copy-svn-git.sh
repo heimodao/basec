1d45157b66bb60168daa0d75b1f03546f36ff30a
wenruo95 <1358124684@qq.com>
2018-02-25 17:12:32 +0800

    test

--- shell/copy-svn-git.sh
+++ shell/copy-svn-git.sh
@@ -1,8 +1,5 @@
 # !/bin/sh
 
 sudo cp -r ~/linkzw.com/algorithm/ ~/Git/
-<<<<<<< HEAD:copy-svn-git.sh
-sudo chown -R linkzw:linkzw ~/Git/algorithm/
-=======
->>>>>>> 730a090c9b6b1227ccf9d968151573f2b7eb09db:shell/copy-svn-git.sh
+#sudo chown -R linkzw:linkzw ~/Git/algorithm/
 echo "copy successed!!!"
