7e63eb4c1592aa14152f45442d6a07583b23bcf8
wenruo95 <1358124684@qq.com>
2018-04-17 12:51:22 +0800

    sync markdown

---
+++ shell/update.sh
@@ -0,0 +1,19 @@
+# linkzw.com
+cd ~/linkzw.com/ && svn update
+
+# algorithm
+cd ~/Git/algorithm/ && git pull
+
+
+# wenruo95.github.io
+cd ~/Git/wenruo95.github.io/ && git pull
+
+
+sudo sh ~/linkzw.com/algorithm/shell/copy-svn-git.sh
+
+sudo sh ~/linkzw.com/algorithm/shell/copy-svn-blog.sh
+
+cd ~/Git/algorithm/ && git status
+
+cd ~/Git/wenruo95.github.io/ && git status
+
