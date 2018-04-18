98e3aa833221d6d61e51625cf5439b8b9e32a472
wenruo95 <1358124684@qq.com>
2018-04-18 18:27:45 +0800

    test

---
+++ shell/commit.sh
@@ -0,0 +1,7 @@
+sudo sh ~/linkzw.com/algorithm/shell/update.sh
+
+cd ~/linkzw.com/ && svn commit -m "$1"
+
+cd ~/Git/algorithm/ && git add . && git commit -a -m "$1"
+
+cd ~/Git/wenruo95.github.io/ && git add . && git commit -a -m "$1"
