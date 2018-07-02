e8db2583c417688f46d26b07beb50faddfd64eed
wenruo95 <1358124684@qq.com>
2018-07-02 23:00:24 +0800

    add cpp-study repository

--- shell/update.sh
+++ shell/update.sh
@@ -16,6 +16,9 @@ cd ~/Git/clogger/ && git pull
 # tools
 cd ~/Git/tools/ && git pull
 
+# c++
+cd ~/Git/cpp-study/ && git pull
+
 sudo sh ~/linkzw.com/algorithm/shell/copy-svn-git.sh
 
 sudo sh ~/linkzw.com/algorithm/shell/copy-svn-blog.sh
@@ -26,10 +29,19 @@ sudo cp -r ~/linkzw.com/tools/ ~/Git/
 
 sudo cp -r ~/linkzw.com/clogger/ ~/Git/
 
+sudo cp -r ~/linkzw.com/algorithm/c++/*.* ~/Git/cpp-study/
+
+pwd
 cd ~/Git/algorithm/ && git status
 
+pwd
 cd ~/Git/wenruo95.github.io/ && git status
 
+pwd
 cd ~/Git/socket-server/ && git status
 
+pwd
 cd ~/Git/clogger/ && git status
+
+pwd
+cd ~/Git/cpp-study/ && git status
