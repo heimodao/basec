eb120276935feb74b7cdb7d43b041363408f92a5
wenruo95 <1358124684@qq.com>
2018-08-04 13:21:09 +0800

    base64

--- base64/base64.c
+++ base64/base64.c
@@ -2,6 +2,17 @@
 
 #include "base64.h"
 
+static unsigned char init_char[64] = {
+	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+	'0','1','2','3','4','5','6','7','8','9','+','/',
+};
+
+// 加密过程中剩余字节数只能为2或者4
+static unsigned int encleft[4] = {0,3,0,15};
+static unsigned int encright[4] = {0,240,0,192};
+
+
 void encrypt_base64(char org[],int len,char result[]) {
 	unsigned int chindex,left,right;
 	unsigned int leave_num = 0, count = 0;
