d85ea1e4953993b0f1d21d062c9bc4c6586ad2ff
wenruo95 <1358124684@qq.com>
2018-08-16 18:45:18 +0800

    day upload

--- test/test-time.c
+++ test/test-time.c
@@ -4,6 +4,7 @@
 int main(void) {
 	time_t now;
 	time(&now);
+	printf("now:%ju\n", now);
 
 	struct tm beg = *localtime(&now);
 
@@ -18,7 +19,7 @@ int main(void) {
 
 	double seconds = difftime(now,mktime(&beg));
 	*/
-	printf("%04d-%02d-%02d",1900 + beg.tm_year,1 + beg.tm_mon,beg.tm_mday);
+	printf("%04d-%02d-%02d\n",1900 + beg.tm_year,1 + beg.tm_mon,beg.tm_mday);
 	printf("step1::%ld\n",mktime(&beg));
 	//printf("%f seconds have passed since the beginning of the month\n",seconds);
 	printf("\nProgram last compiled at %s on %s\n",__TIME__,__DATE__);
