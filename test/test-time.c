6c5a1d1436b50e155f1d023713642b3ac58763ef
wenruo95 <1358124684@qq.com>
2018-08-21 21:33:44 +0800

    time

--- test/test-time.c
+++ test/test-time.c
@@ -7,22 +7,23 @@ int main(void) {
 	printf("now:%ju\n", now);
 
 	struct tm beg = *localtime(&now);
+	printf("%04d-%02d-%02d\n",1900 + beg.tm_year,1 + beg.tm_mon,beg.tm_mday);
+	printf("mktime:%ld\n", mktime(&beg));
 
-	/*
 	// beg
 	beg.tm_hour = 0;
 	beg.tm_min = 0;
 	beg.tm_sec = 0;
 	beg.tm_mday = 1;
-	//beg.tm_mon = 5;
-	//beg.tm_year = 2018;
 
-	double seconds = difftime(now,mktime(&beg));
-	*/
-	printf("%04d-%02d-%02d\n",1900 + beg.tm_year,1 + beg.tm_mon,beg.tm_mday);
-	printf("step1::%ld\n",mktime(&beg));
-	//printf("%f seconds have passed since the beginning of the month\n",seconds);
+	double seconds = difftime(now, mktime(&beg));
+
+	printf("%.f seconds have passed since the beginning of the month\n",seconds);
 	printf("\nProgram last compiled at %s on %s\n",__TIME__,__DATE__);
+
+	time_t epoch = 0;
+	printf("%ld seconds since the epoch began\n", (long) epoch);
+	printf("%s",asctime(gmtime(&epoch)));
 	return 0;
 }
 
